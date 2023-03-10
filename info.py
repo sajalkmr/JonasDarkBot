fdc9e30e210f31a99793525dd3493d18022967db
Sajal <sajal.kumar2003@gmail.com>
2023-03-11 01:58:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,9 +35,10 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-
-DATABASE_URI = f"mongodb+srv://{urllib.parse.quote_plus(username)}:{urllib.parse.quote_plus(password)}@cluster0.inf67vr.mongodb.net/?retryWrites=true&w=majority"
-#DATABASE_URI = quote_plus(environ['DATABASE_URI'])
+#username = 
+#password = 
+#DATABASE_URI = f"mongodb+srv://{urllib.parse.quote_plus(username)}:{urllib.parse.quote_plus(password)}@cluster0.inf67vr.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = quote_plus(environ['DATABASE_URI'])
 DATABASE_NAME = environ.get('DATABASE_NAME', "jonasbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
